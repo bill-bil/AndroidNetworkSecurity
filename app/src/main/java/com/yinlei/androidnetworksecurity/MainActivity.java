ffa1eea624075c2b29b23d57ba3a8c89c7097e01
YinLei <37959310+LeiLeiOfJava@users.noreply.github.com>
2020-08-29 17:17:40 +0800

    搭建客户端

--- app/src/main/java/com/yinlei/androidnetworksecurity/MainActivity.java
+++ app/src/main/java/com/yinlei/androidnetworksecurity/MainActivity.java
@@ -3,12 +3,48 @@ package com.yinlei.androidnetworksecurity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import com.yinlei.androidnetworksecurity.http.HttpRequest;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
+
+    // 服务器地址URL
+    private static final String URL = "";
+
+    private static final String TAG = "yinlei";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final HttpRequest request = new HttpRequest(URL);
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                request.request(new Callback() {
+                    @Override
+                    public void onFailure(@NotNull Call call, @NotNull IOException e) {
+                        Log.e(TAG, "Get请求失败");
+                    }
+
+                    @Override
+                    public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException {
+                        Log.d(TAG, "Get请求成功: "+response.body().string());
+
+                    }
+                });
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/yinlei/androidnetworksecurity/http/HttpRequest.java
@@ -0,0 +1,31 @@
+package com.yinlei.androidnetworksecurity.http;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+
+public class HttpRequest {
+
+    private Call mCall;
+
+    public HttpRequest(String url) {
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                .get()
+                .url(url)
+                .build();
+        mCall = client.newCall(request);
+    }
+
+    public void request(Callback callback) {
+        if (mCall != null) {
+            if (mCall.isExecuted()) {
+                mCall.clone().enqueue(callback);
+            } else {
+                mCall.enqueue(callback);
+            }
+        }
+    }
+
+}
